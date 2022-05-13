7dd961099980132bd4f208bc6d63b5bb3e93924d
Vinicius Campanha <vinicampa@gmail.com>
2022-05-13 19:45:01 -0300

    Adicionando conexão MySQL

--- api.py
+++ api.py
@@ -4,6 +4,8 @@ import sys
 
 import flask 
 
+import mysql.connector
+
 import blueprints.test
 
 from random import choice
@@ -13,6 +15,16 @@ app = flask.Flask(__name__)
 app.secret_key = 'secret'
 
 app.register_blueprint(blueprints.test.blueprint)
+
+conn = mysql.connector.connect(
+    server=os.environ['DB_SERVER'],
+    user=os.environ['DB_USER'],
+    password=os.environ['DB_PASSWORD'],
+    database=os.environ['DB_DATABASE']
+)
+
+conn.autocommit=True
+cursor = conn.cursor()
 
 
 @app.route('/', methods=[ 'GET' ])
@@ -33,7 +45,8 @@ if __name__ == '__main__':
     sys.path.append(current_module)
 
     os.environ['FLASK_APP'] = 'app.py'
-    os.environ['FLASK_ENV'] = 'development'
+    os.environ['FLASK_ENV'] = 'development'   
+    
 
     app.run(host='0.0.0.0',port='8080')
 
