8425c4dcac192acd4baa23a3b4536c4e52c8a433
Vinicius Campanha <vinicampa@gmail.com>
2022-05-13 19:34:57 -0300

    Update api.py

--- api.py
+++ api.py
@@ -2,22 +2,31 @@
 import os
 import sys
 
-import flask
+import flask 
 
-import blueprints.backup
-import blueprints.database
+import blueprints.test
+
+from random import choice
 
 app = flask.Flask(__name__)
 
 app.secret_key = 'secret'
 
-app.register_blueprint(blueprints.backup.blueprint)
-app.register_blueprint(blueprints.database.blueprint)
+app.register_blueprint(blueprints.test.blueprint)
+
 
 @app.route('/', methods=[ 'GET' ])
 def index():
-    return flask.redirect('/index.html')
+    return 'First request'
+
+@app.route('/flipacoin', methods=[ 'GET' ])
+def flipacoin():
+    return choice(['Cara', 'Coroa'])
+
+@app.route('/registername', methods=[ 'POST' ])
+def registername():
     
+
 if __name__ == '__main__':
 
     current_module = os.path.dirname(os.path.curdir)
@@ -27,3 +36,5 @@ if __name__ == '__main__':
     os.environ['FLASK_ENV'] = 'development'
 
     app.run(host='0.0.0.0',port='8080')
+
+    
